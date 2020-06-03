6533404da599c7416e8b37e89139451478184fc1
Carl Mueller <cmuell89@gmail.com>
2020-06-02 17:42:39 -0700

    Python packaging.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='cairo_motion_planning',
+      version='0.1',
+      description='Package containing interfaces and algorithms for motion planning.',
+      url='https://github.com/cairo-robotics/cairo_motion_planning',
+      author='Carl Mueller',
+      author_email='carl.mueller@colorado.edu',
+      license='',
+      packages=find_packages(),
+      install_requires=[])
