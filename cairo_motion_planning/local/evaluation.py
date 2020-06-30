56410bff7e8410a088ed56ef224dd818f8b5e827
Carl Mueller <cmuell89@gmail.com>
2020-06-29 23:46:40 -0700

    Making evaluators functions rather than classes and documentation.

--- cairo_motion_planning/local/evaluation.py
+++ cairo_motion_planning/local/evaluation.py
@@ -1,7 +1,12 @@
 class SubdivisionPathIterator():
+    """
+    An iterator that produces the subdivision selection of elements.
+    
+    a b c d e f g h i j k -> f c i a d g j b e h k
+    """
 
     def __init__(self, local_path):
-        self.segment_queue=[local_path]
+        self.segment_queue = [local_path]
 
     def __iter__(self):
         return self
@@ -10,10 +15,10 @@ class SubdivisionPathIterator():
         if len(self.segment_queue) == 0:
             raise StopIteration
         else:
-            segment=self.segment_queue.pop(0)
-            m_idx=int(len(segment) / 2)
-            s1=segment[:m_idx]
-            s2=segment[m_idx + 1:]
+            segment = self.segment_queue.pop(0)
+            m_idx = int(len(segment) / 2)
+            s1 = segment[:m_idx]
+            s2 = segment[m_idx + 1:]
             if len(s1) > 0:
                 self.segment_queue.append(s1)
             if len(s2) > 0:
@@ -23,28 +28,38 @@ class SubdivisionPathIterator():
             elif len(segment) == 1:
                 return segment[0]
 
-    next=__next__  # python2.x compatibility.
+    next = __next__  # python2.x compatibility.
 
 
-class IncrementalEvaluation():
+def incremental_evaluate(eval_fn, local_path):
+    """
+    Incrementally evaluates a discrete local path i.e. 1-2-3-4-5-6-7-8
 
-    def __init__(self, eval_fn):
-        self.eval_fn=eval_fn
+    Args:
+        eval_fn ([type]): Evaluating function.
+        local_path ([type]): [description]
 
-    def evaluate(self, local_path):
-        for point in local_path:
-            if not self.eval_fun(point):
-                return False
-        return True
+    Returns:
+        [type]: [description]
+    """
+    for point in local_path:
+        if not eval_fn(point):
+            return False
+    return True
 
 
-class SubdividionEvaluation():
+def subdivision_evaluate(eval_fn, local_path):
+    """
+    Evaluates subdivisions of a descrete local path i.e. 4-2-5-1-6-3-7-8
 
-    def __init__(self, eval_fn):
-        self.eval_fn=eval_fn
+    Args:
+        eval_fn ([type]): [description]
+        local_path ([type]): [description]
 
-    def evaluate(self, local_path):
-        for point in SubdivisionPathIterator(local_path):
-            if not self.eval_fun(point):
-                return False
-        return True
+    Returns:
+        [type]: [description]
+    """
+    for point in SubdivisionPathIterator(local_path):
+        if not eval_fn(point):
+            return False
+    return True
